a65099e18329b1485fdb0e96275426dacbdbdea8
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2024-05-10 13:43:03 +0200

    Prune previous assignments

--- tup/src/problem/Pruner.java
+++ tup/src/problem/Pruner.java
@@ -3,12 +3,16 @@ package problem;
 import main.Config;
 
 import java.util.HashSet;
+import java.util.stream.IntStream;
+
+import static main.Config.NUM_UMPIRES;
 
 public class Pruner {
     private final Tree tree;
     private HashSet<Integer> prunedGames;
     private int startRoundForQ1Constraint;
     private int startRoundForQ2Constraint;
+    public static int numPrunedGames = 0;
 
     public Pruner(Tree tree) {
         this.tree = tree;
@@ -22,7 +26,14 @@ public class Pruner {
 
     public HashSet<Integer> pruneGames(int umpire, int currentRoundIndex) {
         initPruner(currentRoundIndex);
+        pruneBasedOnPreviousAssignments(umpire, currentRoundIndex);
+        numPrunedGames += prunedGames.size();
+        System.out.println("NumPruendGames: " + numPrunedGames);
         return prunedGames;
+    }
+
+    public void pruneBasedOnPreviousAssignments(int umpire, int roundIndex) {
+        IntStream.range(0, umpire).mapToObj(uid -> Math.floorMod(tree.umpireScheduleByRound[uid][roundIndex], NUM_UMPIRES)).forEach(prunedGames::add);
     }
 
 }
