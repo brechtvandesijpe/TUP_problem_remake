01a56ad91ab2a659af8bd1ad4c4332e6c85e7003
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2024-05-24 22:21:51 +0200

    Matcher

--- tup/src/problem/Matcher.java
+++ tup/src/problem/Matcher.java
@@ -1,7 +1,31 @@
 package problem;
 
+import model.Instance;
+
 public class Matcher {
 
-    public Matcher(){}
+    private Instance instance;
 
+    public Matcher(Instance instance){
+        this.instance = instance;
+    }
+
+    public void matchRound() {
+        // todo
+    }
+
+    public void solveSubProblem() {
+        // todo
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        return "Matcher{" +
+                "instance=" + instance +
+                '}';
+    }
 }
