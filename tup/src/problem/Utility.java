90d9c7ea912ec2ca820e3d66bf42dc177f58d654
Reinout Annaert <reinout.annaert@student.kuleuven.be>
2024-06-14 17:01:43 +0200

    High Primes

--- tup/src/problem/Utility.java
+++ tup/src/problem/Utility.java
@@ -10,7 +10,19 @@ public class Utility {
     private static final int PRIME = 31;
     private static final int MULTIPLIER = 1223;
     private static final int SET = 1231;
+    // private static final int SET = 17;
+    // private static final int SET = 89;
     private static final int UNSET = 1237;
+    // private static final int UNSET = 7;
+
+    /**
+     * Calculates the vector representation of umpire assignments for a given round.
+     *
+     * @param umpire                The number of umpires.
+     * @param currentRoundIndex     The index of the current round.
+     * @param umpireScheduleByRound The schedule of umpire assignments by round.
+     * @return The vector representation of umpire assignments for the current round.
+     */
 
     public static BitSet calculateVec(int umpire, int currentRoundIndex, int[][] umpireScheduleByRound) {
         return IntStream.rangeClosed(0, umpire).mapToObj(umpireId -> {
@@ -27,23 +39,47 @@ public class Utility {
         });
     }
 
+    /**
+     * Computes a blended digest value using a multiplicative mixing function.
+     * It combines the round number and the hash value of a BitSet.
+     *
+     * @param round The current round number.
+     * @param vec   The BitSet to be hashed.
+     * @return The blended digest value.
+     */
+
     public static int blend(int round, BitSet vec) {
         int digest = 1;
         digest = mixMultiplicative(mixMultiplicative(digest, round), hashBitSet(vec));
         return digest;
     }
 
+    /**
+     * Computes the hash value of a BitSet using a prime number-based hashing function.
+     *
+     * @param vec The BitSet to be hashed.
+     * @return The hash value of the BitSet.
+     */
+
     public static int hashBitSet(BitSet vec) {
         int digest = 1;
         for (int b = 0; b < vec.length(); b++) {
-            digest = PRIME * digest;
+            digest = PRIME * digest + (vec.get(b) ? SET : UNSET);
+            //System.out.println("digest: " + digest);
         }
         return digest;
     }
 
+    /**
+     * Mixes a digest value with a given integer value using a multiplicative mixing function.
+     *
+     * @param digest The current digest value.
+     * @param value  The value to be mixed.
+     * @return The mixed digest value.
+     */
+
     public static int mixMultiplicative(int digest, int value) {
         return MULTIPLIER * digest + value;
     }
-
 
 }
